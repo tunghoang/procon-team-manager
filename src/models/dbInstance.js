3e90666a95b4d674e7256c2958bbdffb362376a7
MrYasuo <buihai0706vn@gmail.com>
2025-11-17 14:25:10 +0700

    chore: Add db port

--- src/models/dbInstance.js
+++ src/models/dbInstance.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require("sequelize");
 
 const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT || 3306;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
+  port: DB_PORT,
   dialect: "mysql",
   logging: false,
 });
